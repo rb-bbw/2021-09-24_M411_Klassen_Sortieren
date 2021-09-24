29c5a0b69bbe6eb116069faa172e9cc2fdf1a7f4
rb-bbw <remo.bergamin@bbwch.onmicrosoft.com>
2021-09-24 15:18:28 +0200

    Do task 1

--- src/ch/bbw/Main.java
+++ src/ch/bbw/Main.java
@@ -1,8 +1,29 @@
 package ch.bbw;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        task_1();
+    }
+    private static void task_1(){
+        int[] data_int = {432, 2342, 1234, 234, 42, 432, 234};
+        Arrays.sort(data_int);
+        System.out.println(Arrays.toString(data_int));
+
+
+        ArrayList<Integer> data_Integer = new ArrayList<>();
+        data_Integer.add(432);
+        data_Integer.add(2342);
+        data_Integer.add(1234);
+        data_Integer.add(234);
+        data_Integer.add(42);
+        data_Integer.add(432);
+        data_Integer.add(234);
+        Collections.sort(data_Integer, Collections.reverseOrder());
+        System.out.println(data_Integer);
     }
 }
